866cc868646da78397c3c3e35abc01aa529e7cff
Elithon Silva <elithonsilva@gmail.com>
2022-01-14 17:06:07 -0300

    Crie o endpoint GET /talker/:id

--- index.js
+++ index.js
@@ -8,13 +8,29 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_ERRO_STATUS = 404;
 const PORT = '3000';
 
 app
   .route('/talker')
   .get((req, res) => {
     const data = JSON.parse(fs.readFileSync(arquivoTalker, 'utf8'));
-    res.status(200).send(data);
+    res.status(HTTP_OK_STATUS).send(data);
+  });
+
+app
+  .route('/talker/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    const data = JSON.parse(fs.readFileSync(arquivoTalker, 'utf8'));
+    const searchId = data.find((pessoaId) => pessoaId.id === Number(id));
+
+    if (!searchId) {
+      res.status(HTTP_ERRO_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+      return;
+    }
+
+    res.status(HTTP_OK_STATUS).json(searchId);
   });
 
 // não remova esse endpoint, e para o avaliador funcionar
