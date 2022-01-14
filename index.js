b44decd9ad3eddcb97136b1400be1023c6373581
Elithon Silva <elithonsilva@gmail.com>
2022-01-14 18:16:40 -0300

    Crie o endpoint POST /login

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const HTTP_OK_STATUS = 200;
 const HTTP_ERRO_STATUS = 404;
 const PORT = '3000';
 
+// Requisito 01
 app
   .route('/talker')
   .get((req, res) => {
@@ -18,6 +19,7 @@ app
     res.status(HTTP_OK_STATUS).send(data);
   });
 
+// Requisito 02
 app
   .route('/talker/:id')
   .get((req, res) => {
@@ -31,6 +33,40 @@ app
     }
 
     res.status(HTTP_OK_STATUS).json(searchId);
+  });
+
+const isEmailValid = (email) => {
+  const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return regexEmail.test(email) === true;
+};
+
+// Requisito 03
+function validationEmailPassword(res, email, password) {
+  if (!email) {
+    res.status(400).json({ message: 'O campo "email" é obrigatório' });
+    return;
+  }
+  if (!isEmailValid(email)) {
+    res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    return;
+  }
+  if (!password) {
+    res.status(400).json({ message: 'O campo "password" é obrigatório' });
+    return;
+  }
+  return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+}
+
+app
+  .route('/login')
+  .post((req, res) => {
+    const { email, password } = req.body;
+    if (isEmailValid(email) && password && password.length >= 6) {
+      res.status(HTTP_OK_STATUS).json({ token: '7mqaVRXJSp886CGr' });
+      return;
+    }
+
+    validationEmailPassword(res, email, password);
   });
 
 // não remova esse endpoint, e para o avaliador funcionar
